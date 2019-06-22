724fff4cc87679c1e6562c4cd2f56be7e740173a
Radim.Langer <radim.langer@kiwi.com>
2019-06-22 19:51:50 +0200

    removed unused properties from API

--- ScrollListDataLoader/Feature/NewsList/Model/Article.swift
+++ ScrollListDataLoader/Feature/NewsList/Model/Article.swift
@@ -12,14 +12,6 @@ struct ArticleResponse: Decodable {
 }
 
 struct Article: Decodable {
-    let source: Source
-    let author: String?
     let title: String
-    let articleDescription: String?
-    let url: String
     let urlToImage: String?
-
-    struct Source: Decodable {
-        let name: String
-    }
 }
